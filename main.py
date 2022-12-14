8ccc8deeba0ae1107981c08ea64d62f43e2a2990
Keycatowo <owo.nthu@outlook.com>
2022-12-14 14:50:15 +0800

    建立專案、新增檔案讀取、檔案基本資訊

---
+++ main.py
@@ -0,0 +1,40 @@
+#%%
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+#%%
+st.header("Music Analysis Tool")
+st.subheader("This tool will help you analyze your music library")
+
+#%%
+with st.expander("上傳檔案(Upload Files)"):
+    file = st.file_uploader("Upload your music library", type=["mp3", "wav", "ogg"])
+    if file is not None:
+        st.subheader("File information")
+        audio_file = st.audio(file, format="audio/ogg")
+        st.write(f"File name: `{file.name}`", )
+        st.write(f"File type: `{file.type}`")
+        st.write(f"File size: `{file.size}`")
+#%%
+# 載入音檔
+import librosa
+import librosa.display
+
+y, sr = librosa.load(file, sr=44100)
+st.write(f"Sample rate: `{sr}`")
+st.write(f"Duration: `{len(y)/sr}`")
+
+
+#%%
+with st.expander("Basic Information"):
+    # 繪製聲音波形圖
+    st.subheader("Waveform")
+    fig, ax = plt.subplots()
+    ax.plot(y)
+
+    st.pyplot(fig)
+    
+    
