58a60e6d53275e54bd90e63fba9e3774ac9f4567
Keycatowo <owo.nthu@outlook.com>
2023-02-19 15:47:12 +0800

    fix: 修正Part6的時間軸刻度問題

--- src/timbre_analysis.py
+++ src/timbre_analysis.py
@@ -13,11 +13,11 @@ def spectral_centroid_analysis(y: npt.ArrayLike, sr: int, save_to_csv: bool = Fa
 
     S, phase = librosa.magphase(librosa.stft(y=y))
     cent = librosa.feature.spectral_centroid(S=S)
-    times = librosa.times_like(cent)
+    times = librosa.times_like(cent, sr)
 
     fig, ax = plt.subplots()
     librosa.display.specshow(librosa.amplitude_to_db(S, ref=np.max),
-                             y_axis='log', x_axis='time', ax=ax)
+                             y_axis='log', x_axis='time', ax=ax, sr=sr)
     ax.plot(times, cent.T, label='Spectral centroid', color='w')
     ax.legend(loc='upper right')
     ax.set(title='log Power spectrogram')
@@ -33,14 +33,14 @@ def rolloff_frequency_analysis(y: npt.ArrayLike, sr: int, save_to_csv: bool = Fa
 
     rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr, roll_percent=roll_percent)
     rolloff_min = librosa.feature.spectral_rolloff(y=y, sr=sr, roll_percent=0.01)
-    times = librosa.times_like(rolloff)
+    times = librosa.times_like(rolloff, sr)
     S, phase = librosa.magphase(librosa.stft(y))
 
     fig, ax = plt.subplots()
     librosa.display.specshow(librosa.amplitude_to_db(S, ref=np.max),
-                             y_axis='log', x_axis='time', ax=ax)
-    ax.plot(librosa.times_like(rolloff), rolloff[0], label=f'Roll-off frequency ({roll_percent})')
-    ax.plot(librosa.times_like(rolloff), rolloff_min[0], color='w',
+                             y_axis='log', x_axis='time', ax=ax, sr=sr)
+    ax.plot(librosa.times_like(rolloff,sr), rolloff[0], label=f'Roll-off frequency ({roll_percent})')
+    ax.plot(librosa.times_like(rolloff,sr), rolloff_min[0], color='w',
             label='Roll-off frequency (0.01)')
     ax.legend(loc='lower right')
     ax.set(title='log Power spectrogram')
@@ -55,16 +55,16 @@ def spectral_bandwidth_analysis(y: npt.ArrayLike, sr: int, save_to_csv: bool = F
     
     S, phase = librosa.magphase(librosa.stft(y=y))
     spec_bw = librosa.feature.spectral_bandwidth(S=S)
-    times = librosa.times_like(spec_bw)
+    times = librosa.times_like(spec_bw, sr)
 
     fig, ax = plt.subplots(nrows=2, sharex=True)
-    centroid = librosa.feature.spectral_centroid(S=S)
+    centroid = librosa.feature.spectral_centroid(S=S, sr=sr)
     ax[0].semilogy(times, spec_bw[0], label='Spectral bandwidth')
     ax[0].set(ylabel='Hz', xticks=[], xlim=[times.min(), times.max()])
     ax[0].legend()
     ax[0].label_outer()
     librosa.display.specshow(librosa.amplitude_to_db(S, ref=np.max),
-                             y_axis='log', x_axis='time', ax=ax[1])
+                             y_axis='log', x_axis='time', ax=ax[1], sr=sr)
     ax[1].set(title='log Power spectrogram')
     ax[1].fill_between(times, np.maximum(0, centroid[0] - spec_bw[0]),
                        np.minimum(centroid[0] + spec_bw[0], sr/2),
@@ -89,19 +89,19 @@ def harmonic_percussive_source_separation(y: npt.ArrayLike, sr: int) -> None :
     # 設置子圖之間的水平間距和垂直間距
     plt.subplots_adjust(hspace=0.6, wspace=0.3)
     img = librosa.display.specshow(librosa.amplitude_to_db(np.abs(D),ref=np.max),
-                                   y_axis='log', x_axis='time', ax=ax[0])
+                                   y_axis='log', x_axis='time', ax=ax[0], sr=sr)
     ax[0].set(title='Full power spectrogram')
     #// ax[0].label_outer()
     ax[0].set_xlabel('') # 不顯示x軸名稱
 
     librosa.display.specshow(librosa.amplitude_to_db(np.abs(H), ref=np.max(np.abs(D))),
-                             y_axis='log', x_axis='time', ax=ax[1])
+                             y_axis='log', x_axis='time', ax=ax[1], sr=sr)
     ax[1].set(title='Harmonic power spectrogram')
     #// ax[1].label_outer()
     ax[1].set_xlabel('') # 不顯示x軸名稱
 
     librosa.display.specshow(librosa.amplitude_to_db(np.abs(P), ref=np.max(np.abs(D))),
-                             y_axis='log', x_axis='time', ax=ax[2])
+                             y_axis='log', x_axis='time', ax=ax[2], sr=sr)
     ax[2].set(title='Percussive power spectrogram')
     fig.colorbar(img, ax=ax, format='%+2.0f dB')
 
